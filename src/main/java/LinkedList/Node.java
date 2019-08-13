ff8d3ca5a0a5b2ba0706d07c752471e704a2144f
Ahmad Raza <razatastic@gmail.com>
2019-08-13 12:23:43 -0400

    Update Javadoc

--- src/main/java/LinkedList/Node.java
+++ src/main/java/LinkedList/Node.java
@@ -4,6 +4,11 @@ class Node {
     String data;
     Node next;
 
+    /**
+     * Constructor
+     *
+     * @param data initial value of node
+     */
     Node(String data) {
         this.data = data;
         this.next = null;
