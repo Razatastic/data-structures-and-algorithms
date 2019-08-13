67b660613a4b5e55db931c62ec14aa743ca3dc86
Ahmad Raza <razatastic@gmail.com>
2019-08-13 17:44:19 -0400

    Add populateList() for testing purposes

--- src/test/java/LinkedList/LinkedListTest.java
+++ src/test/java/LinkedList/LinkedListTest.java
@@ -6,45 +6,52 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+
 class LinkedListTest {
-    private static LinkedList test = new LinkedList("hello"); // with initial value for head node
+    private static LinkedList test = new LinkedList("one"); // with initial value for head node
     private static LinkedList test2 = new LinkedList(); // without initial value for head node
+
+    void populateList() {
+        test.add("three");
+        test.add("five");
+        test.add("seven");
+    }
 
     @Test
     @DisplayName("Checks whether the head is properly being initialized in the constructor")
     void headIsInitialized() {
-        assertEquals("hello", test.peek());
+        assertEquals("one", test.peek());
     }
 
     @Test
     void getHead() throws OutOfRangeException {
-        assertEquals("hello", test.get(0));
+        assertEquals("one", test.get(0));
     }
 
     @Test
     void getAnyElement() throws OutOfRangeException {
-        test.add("yo");
-        assertEquals("yo", test.get(1));
+        populateList();
+        assertEquals("five", test.get(test.size() - 2));
     }
 
     @Test
     @DisplayName("Checks if the input is being added to the end of the list")
     void add() throws OutOfRangeException {
-        test.add("world");
-        assertEquals("world", test.getLast());
+        test.add("nine");
+        assertEquals("nine", test.getLast());
     }
 
     @Test
     void addAtFront() {
-        test.addFirst("Yo");
-        assertEquals("Yo", test.peek());
+        test.addFirst("zero");
+        assertEquals("zero", test.peek());
     }
 
     @Test
     void addAtIndex() throws OutOfRangeException {
-        test.add("world");
-        test.add(1, "testing");
-        assertEquals("testing", test.get(1));
+        test.add(1, "two");
+        assertEquals("two", test.get(1));
     }
 
     @Test
