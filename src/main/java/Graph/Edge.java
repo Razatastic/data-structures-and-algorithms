08b0cd1edc6e9dcc1c2611f0efa42bba973ba2dc
Ahmad Raza <raza2157@gmail.com>
2022-12-13 03:40:41 -0500

    Update plugins & cleanup

--- src/main/java/Graph/Edge.java
+++ src/main/java/Graph/Edge.java
@@ -1,5 +1,7 @@
 package Graph;
 
+import java.util.Objects;
+
 public class Edge<T, V extends Comparable<V>> implements Comparable<Edge<T, V>> {
     private final Node<T> start, end;
     private final V weight;
@@ -29,9 +31,11 @@ public class Edge<T, V extends Comparable<V>> implements Comparable<Edge<T, V>> 
 
     @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Edge)) return false;
-        Edge<T, V> castedInput = (Edge<T, V>) o;
-        return castedInput.getStart().equals(this.getStart()) && (castedInput.getEnd().equals(this.getEnd()));
+        if (this == o) return true;
+        if (!(o instanceof Edge<?, ?> that)) return false;
+
+        return Objects.equals(this.start, that.start)
+                && Objects.equals(this.end, that.end);
     }
 
     @Override
